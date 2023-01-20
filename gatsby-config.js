2b81d26aee4d3e3a37787f978b1dae33ca749a7c
APAP-PDC\lsvasquez <lsvasquez@apap.com.do>
2023-01-20 16:55:07 -0400

    WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,14 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `BittBot`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,9 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
-
+import "../style.scss"
+import Header from "../components/header"
+import { Container, Row, Col, Button } from "reactstrap"
 const links = [
   {
     text: "Tutorial",
@@ -71,50 +72,44 @@ const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=de
 
 const IndexPage = () => (
   <Layout>
-    <div className={styles.textCenter}>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-      <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
-        ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
-    </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))}
+    <Container>
+      <Row xs="2">
+        <Col>
+          <div className="hero">
+            <h2 className="hero-title">
+              Con <span className="principal-color">ChatBot</span>, automatizar
+              el servicio al cliente{" "}
+              <span className="principal-color">es muy fácil</span>
+            </h2>
+            <p className="text">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+              erat volutpat.
+            </p>
+            <div className="btn-container">
+              <Button className="btn-primary">¡Quiero probarlo!</Button>
+              <Button className="btn-secondary">Demo</Button>
+            </div>
+          </div>
+        </Col>
+        <Col lg='6'>
+          <div className="image-container">
+            <StaticImage
+              src="../images/bittbot.png"
+              className="hero-image"
+              loading="eager"
+              quality={50}
+              width={600}
+              formats={["auto", "webp", "avif"]}
+              breakpoints={[750, 1080, 1366, 1920]}
+              alt="BittBot"
+              placeholder="BittBot"
+              backgroundColor="transparent"
+            />
+          </div>
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 )
 
